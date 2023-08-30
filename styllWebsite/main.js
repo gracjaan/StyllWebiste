28b176f2a3eb64918fbfc569745c2fd2147e7aad
gracjaan <gchmielnicki3005@gmail.com>
2023-08-30 14:49:58 +0200

    Added pagination to navabr

--- styllWebsite/main.js
+++ styllWebsite/main.js
@@ -42,3 +42,53 @@ dots.forEach((dot, index) => {
     });
   });
 });
+
+
+
+
+
+const screens = document.querySelectorAll('.home-wrapper');
+const indicators = document.querySelectorAll('.navbar-text');
+
+// Create an Intersection Observer to track the visibility of pages
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Find the index of the visible page
+        const pageIndex = Array.from(screens).indexOf(entry.target);
+
+        // Update the selected dot
+        indicators.forEach((indicator, index) => {
+          if (index === pageIndex) {
+            indicator.classList.add('navbar-text-selected');
+            indicator.classList.remove('navbar-text');
+          } else {
+            indicator.classList.remove('navbar-text-selected');
+            indicator.classList.add('navbar-text');
+          }
+        });
+      }
+    });
+  },
+  {
+    root: document.querySelector('.scrollable-mid-wrapper'),
+    rootMargin: '0px',
+    threshold: 0.5, // Adjust this threshold as needed
+  }
+);
+
+// Observe each page
+screens.forEach((screen) => {
+  observer2.observe(screen);
+});
+
+// Add click event listeners to the dots
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', () => {
+    // Scroll to the corresponding page
+    screens[index].scrollIntoView({
+      behavior: 'smooth', // Use 'auto' for immediate scroll
+    });
+  });
+});
